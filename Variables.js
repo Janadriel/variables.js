6adff76891f141afcb9ff78c17abb0126d8b0b7c
Janadriel <pjanadriel@gmail.com>
2024-09-09 09:44:32 +0800

    Create Variables.js

---
+++ Variables.js
@@ -0,0 +1,17 @@
+// Create a variable named name and store your name
+let name = 'Jan Adriel Pilayan';
+
+// Create a variable named whatDoYouWannaBecomeInYourLife and store "programmer"
+let whatDoYouWannaBecomeInYourLife = 'programmer';
+
+// Create a variable named gender and store your gender
+let gender = 'Male';
+
+// Create a variable named Discordhandle and store your Discord Handle
+let Discordhandle = 'JA';
+
+// Log all the variables to the console
+console.log('What is your Name? ', name);
+console.log('What do you wanna become in your life? ',whatDoYouWannaBecomeInYourLife);
+console.log('Are you Male or Female? ', gender);
+console.log('My Discord: ', DiscordHandle);
